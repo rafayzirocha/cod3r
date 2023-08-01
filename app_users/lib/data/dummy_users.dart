d0d1b484997216445ea233f5456b8e70c951fa9f
Rafael Rocha <rafayzirocha@gmail.com>
2023-08-01 14:57:44 -0300

    added the avatars and created the user repository

---
+++ app_users/lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import 'package:app_users/models/user.dart';
+
+const dummyUsers = {
+  '1': User(
+      id: '1',
+      name: 'Rafael',
+      email: 'rafael@gmail.com',
+      avatarUrl: 'images/dog.png'),
+  '2': User(
+      id: '2',
+      name: 'Arthur',
+      email: 'arthur@gmail.com',
+      avatarUrl: 'images/drinking_coffee.png'),
+  '3': User(
+      id: '3',
+      name: 'Maria',
+      email: 'maria@gmail.com',
+      avatarUrl: 'images/old_person.png'),
+  '4': User(
+      id: '4',
+      name: 'Gabriela',
+      email: 'gabriela@gmail.com',
+      avatarUrl: 'images/teacher.png'),
+};
